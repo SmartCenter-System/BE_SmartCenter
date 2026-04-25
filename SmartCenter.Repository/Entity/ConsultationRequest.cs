c538264fc9d7bb747218d6cd84dfca42a3ad1aa2
VoGiaHuy <huyvog6226@gmail.com>
2026-04-25 14:47:01 +0700

    feat: add entity ConsultationReqStatus

--- SmartCenter.Repository/Entity/ConsultationRequest.cs
+++ SmartCenter.Repository/Entity/ConsultationRequest.cs
@@ -1,6 +1,28 @@
-﻿namespace SmartCenter.Repository.Entity;
+﻿using SmartCenter.Repository.Abtraction;
+using SmartCenter.Repository.Entity.Enums;
 
-public class ConsultationRequest
+namespace SmartCenter.Repository.Entity;
+
+public class ConsultationRequest: BaseEntity<Guid>, IAuditableEntity
 {
     
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
+    
+    public Guid CourseId { get; set; }
+    public Course? Course { get; set; }
+    
+    public Guid? StaffId { get; set; }
+    
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public required string Email { get; set; }
+    public required string PhoneNumber { get; set; }
+    public string? Message { get; set; }
+    public ConsultReqStatus Status { get; set; }   
+    public string? Notes { get; set; }
+    
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+
 }
--- SmartCenter.Service/obj/Debug/net8.0/SmartCenter.Service.AssemblyInfo.cs
+++ SmartCenter.Service/obj/Debug/net8.0/SmartCenter.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCenter.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3760875950f16484d840855dda4a27c56e27d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fec6f49282ce9616cbaffa6dbb97a999b9970e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCenter.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCenter.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
