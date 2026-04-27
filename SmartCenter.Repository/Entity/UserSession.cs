9e0608cfc6c137deebdeffe02865db20e85bf8e1
VoGiaHuy <huyvog6226@gmail.com>
2026-04-27 15:53:25 +0700

    feat: add entity UserSession

---
+++ SmartCenter.Repository/Entity/UserSession.cs
@@ -0,0 +1,17 @@
+﻿using SmartCenter.Repository.Abtraction;
+
+namespace SmartCenter.Repository.Entity;
+
+public class UserSession: BaseEntity<Guid>, IAuditableEntity
+{
+    public Guid  UserId { get; set; }
+    public User? User { get; set; }
+    
+    public required string DeviceFingerprint { get; set; }
+    public required string RefreshToken { get; set; }
+    public required DateTimeOffset ExpiresAt { get; set; }
+    public required bool IsRevoked { get; set; }
+    
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
