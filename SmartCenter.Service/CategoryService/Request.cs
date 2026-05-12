4d78cf3c4e593f408c3c6619382fb43e48335a36
duongbilly18012004@gmail.com <duongbilly18012004@gmail.com>
2026-05-12 11:14:23 +0700

    hehe

--- SmartCenter.Service/CategoryService/Request.cs
+++ SmartCenter.Service/CategoryService/Request.cs
@@ -2,5 +2,19 @@ namespace SmartCenter.Service.CategoryService;
 
 public class Request
 {
+    public class UpDateCategoryRequest()
+    {
+        public Guid CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public string? CategoryDescription { get; set; }
+        public string? CategoryIConUrl { get; set; }
+        public bool? isActive { get; set; }
+    }
     
+    public class CreateCategoryRequest()
+    {
+        public required string CategoryName { get; set; }
+        public required string CategoryDescription { get; set; }
+        public required string CategoryIConUrl { get; set; }
+    }
 }
